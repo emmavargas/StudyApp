1680577496ade9388bb7047a01075b8d537d635a
Mariano Ocaranza <marianoocaranza@gmail.com>
2025-04-09 11:26:45 -0300

    agregado un logo e iconos. correccion menor en posicion de formularios de autenticacion

--- src/main/resources/static/assets/js/nav.js
+++ src/main/resources/static/assets/js/nav.js
@@ -1,40 +1,24 @@
 const navcenter = document.getElementById("nav-center")
 const navcentermobile = document.getElementById("nav-center-mobile")
 const burger = document.getElementById('burger');
+const menuIcon = document.getElementById('open-menu')
 const mobileMenu = document.getElementById('mobilenav');
-const closeButton = document.getElementById('close-menu')
 const options = document.querySelectorAll('#menu-option-mobile')
 
-console.log(options)
 burger.addEventListener('click', () => {
+    if (mobileMenu.classList.contains('active')) {
+        menuIcon.src = './assets/img/menu.svg'
+    }
+    else {
+        menuIcon.src = './assets/img/close.svg'
+    }
     mobileMenu.classList.toggle('active');
-    closeButton.classList.toggle('active');
 });
-
-closeButton.addEventListener('click', ()=> {
-    mobileMenu.classList.remove('active');
-    closeButton.classList.remove('active')
-})
 
 options.forEach((option)=> {
     option.addEventListener('click', ()=>{
         mobileMenu.classList.remove('active');
-        closeButton.classList.remove('active')
+        menuIcon.src = './assets/img/menu.svg'
     })
 })
-
-window.onresize=()=> {
-    if(window.innerWidth >= 768) {
-        open.style.display = 'none';
-        close.style.display = 'none';
-        navcenter.style.display = 'flex';
-        navcentermobile.style.display = 'none';
-    }
-    else {
-        open.style.display = 'flex';
-        close.style.display = 'none';
-        navcenter.style.display = 'none';
-        navcentermobile.style.display = 'none';
-    }
-};
 
